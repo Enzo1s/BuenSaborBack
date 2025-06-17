881af673639b7f5f8ed0b23d3f93788fb2f7aad5
Joaquín Román <moleroman@hotmail.com.ar>
2025-06-17 02:21:47 -0300

    Actualiza MercadoPago

--- src/main/java/com/buenSabor/entity/DatosMercadoPago.java
+++ src/main/java/com/buenSabor/entity/DatosMercadoPago.java
@@ -1,32 +1,46 @@
 package com.buenSabor.entity;
 
 import java.io.Serializable;
-
-import java.time.LocalDate;
-
 import jakarta.persistence.Id;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Data
-@EqualsAndHashCode(callSuper=false)
+@EqualsAndHashCode(callSuper = false)
 @Entity
-public class DatosMercadoPago extends ABM implements Serializable{
+public class DatosMercadoPago extends ABM implements Serializable {
 
 	private static final long serialVersionUID = -8045116941495778953L;
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.UUID)
 	private String id;
-	private LocalDate dateCreated;
-	private LocalDate dateApproved;
-	private LocalDate dateLastUpdated;
-	private String paymentTypeId;
-	private String paymentMethodId;
-	private String status;
-	private String statusDetail;
 
+	private String collectionId;
+
+	private String collectionStatus;
+
+	private String paymentId;
+
+	private String status;
+
+	@Column(nullable = true)
+	private String externalReference;
+
+	private String paymentType;
+
+	private String merchantOrderId;
+
+	private String preferenceId;
+
+	private String siteId;
+
+	private String processingMode;
+
+	@Column(nullable = true)
+	private String merchantAccountId;
 }
